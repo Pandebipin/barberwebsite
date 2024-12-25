bba3384209d59b8a86942dbf2403cab4ba78b003
bipin pandey <bp9777643@gmail.com>
2024-12-25 18:02:01 +0545

    making res

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,9 +8,9 @@ function Contact() {
     <div className="md:h-screen mx-auto p-3">
       <h1 className="text-xl text-white text-center py-6">Contact Us</h1>
       <div className="flex flex-col md:flex-row  gap-5 p-2 ">
-        <div className="map md:w-[60vw] px-5">
+        <div className="map md:w-1/2 px-5">
           <iframe
-            className="rounded-lg w-[260px] sm:w-[350px] md:w-[450px] lg:w-[450px] xl:w-[550px]"
+            className="rounded-lg w-full h-[350px] md:h-[400px]"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d700.3455905070192!2d83.03695737006608!3d27.66103836716012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3996f9d70b40613f%3A0x3855c12c1e22b8e!2sSiddhartha%20Bank%20Limited%2C%20Gorusinghe%20Branch!5e0!3m2!1sen!2snp!4v1732964286673!5m2!1sen!2snp"
             height="350"
             allowfullscreen=""

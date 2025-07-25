efa8fcb9ab300659457ac3eef395e00b79d01a90
bipin pandey <bp9777643@gmail.com>
2025-07-25 14:24:12 +0545

    updating compo info

--- src/components/Redeem-Card/RedeemCard.jsx
+++ src/components/Redeem-Card/RedeemCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsStarFill } from "react-icons/bs";
 import { FiScissors } from "react-icons/fi";
 
-function RedeemCard({ heading, service, feature, reedemAmt, icon }) {
+function RedeemCard({ service, feature, reedemAmt, icon }) {
   return (
     <div className="group md:w-[310px] bg-[#251E18] p-4 rounded-lg hover:-translate-y-2 border border-transparent hover:border-yellow-500 hover:shadow-[0_0_15px_rgba(255,255,0,0.3)] transition-all duration-300 ease-in-out">
       <div className="flex w-full justify-between items-center pb-3">
@@ -13,16 +13,14 @@ function RedeemCard({ heading, service, feature, reedemAmt, icon }) {
       </div>
 
       <div className="sec flex flex-col gap-2 py-4">
-        <h4 className="text-lg text-white">Premium Haircut</h4>
-        <p className="text-gray-400 md:text-nowrap">
-          Signature cut with hot towel treatment
-        </p>
+        <h4 className="text-lg text-white">{service}</h4>
+        <p className="text-gray-400 md:text-nowrap">{feature}</p>
       </div>
 
       <div className="flex justify-between p-2 items-center">
         <div className="flex gap-1 items-center">
           <BsStarFill className="text-yellow-400 text-semibold" />
-          <p className="text-yellow-500 font-semibold">500</p>
+          <p className="text-yellow-500 font-semibold">{reedemAmt}</p>
         </div>
         <button className="bg-[#db771f] rounded-md shadow-lg px-4 py-2">
           Reedem
